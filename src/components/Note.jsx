4f63adc9457337eaeae6bb673119587135917640
Cheshta17 <122281116+Cheshta17@users.noreply.github.com>
2023-07-15 00:14:03 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+const react = require("react");
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>NoteUnlock</h1>
+      <p>Unleash your Ideas & Ignite your creativity!</p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,7 @@
+const react = require("react");
+
+function Paragraph() {
+  return <p>Hello dbhsb</p>;
+}
+
+export default Paragraph;
